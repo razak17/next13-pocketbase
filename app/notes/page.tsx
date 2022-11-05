ef6ce4c0dd4c54d0c4d670fab79927c44d08a640
Razak <moabdulrazak17@gmail.com>
2022-11-05 06:49:26 +0000

    chore(note): create note

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import CreateNote from "./CreateNote";
 import styles from "./Notes.module.css";
 
 async function getNotes() {
@@ -21,8 +22,9 @@ export default async function NotesPage() {
           return <Note key={note.id} note={note} />;
         })}
       </div>
+      <CreateNote />
     </div>
-  )
+  );
 }
 
 function Note({ note }: any) {
