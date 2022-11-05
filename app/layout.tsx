eac53e712b08c9e6939e58c2c275bcf3ae65628a
Razak <moabdulrazak17@gmail.com>
2022-11-05 06:21:24 +0000

    chore: project setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-head-element */
+import Link from "next/link";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head>
+        <title>Notes App With Next 13</title>
+      </head>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,7 @@
+export default function NotesPage() {
+  return (
+    <div>
+      <h1>Notes Page</h1>
+    </div>
+  );
+}
